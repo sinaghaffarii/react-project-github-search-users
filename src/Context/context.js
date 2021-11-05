9d630dbec223f7439a19d5ce00acae0c4ae1887f
sinaghaffarii <sinaghafari.dev@gmail.com>
2021-11-05 23:21:10 +0330

    complete search component and fix repos/followers component

--- src/Context/context.js
+++ src/Context/context.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import mockFollowers from './mockData/mockFollowers';
 import mockRepos from './mockData/mockRepos';
 import mockUser from './mockData/mockUser';
+import axios from 'axios';
 
 
 const rootUrl = 'https://api.github.com';
@@ -15,9 +16,67 @@ const GithubProvider = ({ children }) => {
     const [repos, setRepos] = useState(mockRepos)
     const [followers , setFollowers] = useState(mockFollowers)
 
+    //  Request Loading
+    const [requests, setRequests] = useState(0)
+    const [isLoading, setIsLoading] = useState(false)
+
+    // error
+
+    const [error , setError] = useState({show: false, msg: ''})
+
+        //  search github user
+
+        const searchGithubUser = async (user) => {
+            toggleError()
+            setIsLoading(true)
+            const response = await axios(`${rootUrl}/users/${user}`)
+            .catch((err) => console.log(err))
+            if(response) {
+                setGithubUser(response.data)
+                const {login , followers_url} = response.data 
+                // repos
+                axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) => 
+                setRepos(response.data)
+                )
+                  // followers
+                  axios(`${followers_url}?per_page=100`).then((response) => {
+                    setFollowers(response.data)
+                })
+            }else {
+                toggleError(true, 'there is no user with that username')
+            }
+            checkRequests()
+            setIsLoading(false)
+        }
+
+    // check Rate
+
+    const checkRequests = () => {
+        axios( `${rootUrl}/rate_limit`)
+        .then(({data}) => {
+            let {rate: {remaining}}= data
+            setRequests(remaining)
+            if(remaining === 0) {
+                toggleError(true, 'sorry, you have exceeded your  hourly rate limit!')
+            }
+        })
+        .catch((err) => console.log(err))
+    }
+
+    function toggleError(show = false, msg = '') {
+        setError({show, msg})
+    }
+
+
+
+    // error
+
+    useEffect(() => {
+        checkRequests()
+    },[])
 
     return ( 
-        <GithubContext.Provider value = { {githubUser, repos, followers}} > 
+        <GithubContext.Provider value = { {githubUser, repos, followers, requests, error, isLoading, searchGithubUser}} > 
         {children} 
         </GithubContext.Provider>
     )
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import {Navbar, Search, Info, User, Repos} from '../Components'
+import { GithubContext } from '../Context/context'
+import ScaleLoader from 'react-spinners/ScaleLoader'
+
+
 
 const Dashboard = () => {
+
+
+
+  const {isLoading} = React.useContext(GithubContext)
+
+  if(isLoading) {
+    return(
+      <main>
+        <Navbar/>
+        <Search/>
+        <div className="spinnerStyle" >
+        <ScaleLoader color={'#364F6B '} />
+        </div>
+      </main>
+    )
+  }
+
   return (
     <div>
-      {/* <Navbar></Navbar> */}
-      {/* <Search/> */}
+      <Navbar></Navbar>
+      <Search/>
       <Info/>
       <User/>
       <Repos/>
